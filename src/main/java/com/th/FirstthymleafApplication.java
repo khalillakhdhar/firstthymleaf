59e76a3ffd1b4158d57e3533ac85ad41d6a19a2e
khalil <khalillakhdharatc@gmail.com>
2022-03-24 09:22:57 +0100

    Initial commit

---
+++ src/main/java/com/th/FirstthymleafApplication.java
@@ -0,0 +1,13 @@
+package com.th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstthymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstthymleafApplication.class, args);
+	}
+
+}
